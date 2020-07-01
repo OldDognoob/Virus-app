7a437154dd5b4a1128811005c43dc9d441c011c8
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-07-01 11:42:38 +0300

    card file restructured

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import {Cards, Chart, CountryPicker} from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api/";
 
 class App extends React.Component {
-
-    async componentDidMount(){
-        const data = await fetchData();
-
-        console.log(data);
+    state = {
+        data:{},
     }
-    render(){
-        return (
-            <div className={styles.container}>
-                <Cards/>
-                <CountryPicker/>
-                <Chart/>
-            </div>
-        )
-    }
+    async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({data});
+  }
+  render() {
+      const { data } = this.state;
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 export default App;
--- covid-app/src/components/Cards/Cards.jsx
+++ covid-app/src/components/Cards/Cards.jsx
@@ -1,6 +1,37 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import styles from './Card.module.css';
 
-const Cards = () => {
-  return <h1>Cards</h1>;
+const Cards = (props) => {
+    console.log(props);
+
+  return <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography variant="h5">REAL DATA</Typography>
+                <Typography color="textSecondary">REAL DATA</Typography>
+                <Typography variant="body2"> Number of active cases of VIRUS COVID</Typography>
+            </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography variant="h5">REAL DATA</Typography>
+                <Typography color="textSecondary">REAL DATA</Typography>
+                <Typography variant="body2"> Number of active cases of VIRUS COVID</Typography>
+            </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography variant="h5">REAL DATA</Typography>
+                <Typography color="textSecondary">REAL DATA</Typography>
+                <Typography variant="body2"> Number of active cases of VIRUS COVID</Typography>
+            </CardContent>
+        </Grid>
+      </Grid>
+  </div>
 };
 export default Cards;
